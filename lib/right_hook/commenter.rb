4b4428f355a315881d41a9707d378cebb38724a3
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-09-30 16:04:35 -0700

    Log result of adding comment

--- lib/right_hook/commenter.rb
+++ lib/right_hook/commenter.rb
@@ -1,11 +1,13 @@
 require 'right_hook/authenticated_client'
+require 'right_hook/logger'
 require 'octokit'
 
 module RightHook
   # Provides an interface for adding comments on GitHub
   class Commenter < AuthenticatedClient
     def comment_on_issue(owner, repo_name, issue_number, comment)
-      client.add_comment("#{owner}/#{repo_name}", issue_number, comment)
+      result = client.add_comment("#{owner}/#{repo_name}", issue_number, comment)
+      RightHook.logger.info("Result of comment_on_issue: #{result}")
     end
   end
 end
