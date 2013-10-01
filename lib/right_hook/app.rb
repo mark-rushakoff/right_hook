9c7ade352916d5532177728a2d9307cc2990e0a4
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-09-30 22:48:23 -0700

    Expect correct header now

--- lib/right_hook/app.rb
+++ lib/right_hook/app.rb
@@ -2,7 +2,6 @@ require 'sinatra/base'
 require 'json'
 
 require 'right_hook/event'
-require 'right_hook/logger'
 
 module RightHook
   class App < Sinatra::Base
@@ -35,14 +34,9 @@ module RightHook
 
       # http://pubsubhubbub.googlecode.com/git/pubsubhubbub-core-0.4.html#authednotify
       # "If the signature does not match, subscribers MUST still return a 2xx success response to acknowledge receipt, but locally ignore the message as invalid."
-      received_signature = request.env['X-Hub-Signature']
+      received_signature = request.env['HTTP_X_HUB_SIGNATURE']
       calculated_signature = "sha1=#{expected_signature}"
-      unless received_signature == calculated_signature
-        RightHook.logger.warn('Signature mismatch')
-        RightHook.logger.warn("Received:   #{received_signature}")
-        RightHook.logger.warn("Calculated: #{calculated_signature}")
-        halt 202, "Signature mismatch"
-      end
+      halt 202, "Signature mismatch" unless received_signature == calculated_signature
     end
   end
 end
--- spec/support/spec_helpers.rb
+++ spec/support/spec_helpers.rb
@@ -4,7 +4,8 @@ module RightHook
   module SpecHelpers
     def generate_secret_header(secret, body)
       sha = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'), secret, body)
-      {'X-Hub-Signature' => "sha1=#{sha}"}
+      # GitHub sends it as 'X-Hub-Signature', but Rack provides it as HTTP_X_HUB_SIGNATURE... :/
+      {'HTTP_X_HUB_SIGNATURE' => "sha1=#{sha}"}
     end
   end
 end
